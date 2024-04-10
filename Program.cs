d3dc14d27384a293a61f054fb0a1eaf76bf3def6
marcescala <marcescala@yahoo.com>
2024-04-10 08:50:05 -0500

    entorno de trabajo C#

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Guess_the_number;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("What is your name?");
+        var name = Console.ReadLine();
+        var currentDate = DateTime.Now;
+        Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+        Console.Write($"{Environment.NewLine}Press any key to exit...");
+        Console.ReadKey(true);
+    }
+}
