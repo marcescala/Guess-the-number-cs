e5db5bbebed33e61b3957ba0203eeac557efe136
marcescala <marcescala@yahoo.com>
2024-04-24 15:27:00 -0500

    nuevas rutas test

---
+++ Guess-the-number.Tests/GameTest.cs
@@ -0,0 +1,52 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GuessTheNumber;
+
+
+namespace Guess_the_number.Tests
+{
+    [TestClass]
+    public class GameTest
+    {
+         [TestMethod]
+    public void TestGuessIsLowerThanRandom()
+    
+        [TestMethod]
+        public void TestCheckGuess_LowerThanNumber()
+        {
+
+            Game game = new Game("TestPlayer");
+            int guess = game.NumberRandom - 1; // el intento de la jugadora es menor que el número ramdom
+
+            bool result = game.CheckGuess(guess);
+
+            Assert.IsTrue(result); // Esperamos que la función devuelva true
+
+        }
+
+        [TestMethod]
+        public void TestCheckGuess_HigherThanNumber()
+        {
+
+            Game game = new Game("TestPlayer");
+            int guess = game.NumberRandom + 1; // el intento de la jugadora es mayor que el número ramdom
+
+            bool result = game.CheckGuess(guess);
+
+            Assert.IsTrue(result); // Esperamos que la función devuelva true
+
+        }
+
+        [TestMethod]
+        public void TestCheckGuess_CorrectGuess()
+        {
+
+            Game game = new Game("TestPlayer");
+            int guess = game.NumberRandom; // Suponemos que el intento de la jugador es igual al número ramdom
+
+            bool result = game.CheckGuess(guess);
+
+            Assert.IsTrue(result); // Esperamos que la función devuelva true
+
+        }
+    }
+}
